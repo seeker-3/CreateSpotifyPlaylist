8e6b24b633252234bc5179b678351f526bc613ac
Abram Bradley <abramabradley@gmail.com>
2021-09-24 00:19:45 -0400

    cleaned up request file

--- api/request.js
+++ api/request.js
@@ -1,21 +1,14 @@
-const
-  root = './../',
+const root = './../',
   util = root + 'util/',
   auth = root + 'auth/',
-  {
-    read,
-  } = require(util + 'fs'),
-
+  { read } = require(util + 'fs'),
   axios = require('axios'),
   getAuth = require(auth + 'get_auth_token')
-;
-
 function cleanLastfm(data) {
-  return data;
+  return data
 }
 
 function cleanSpotify(data) {
-
   const keys = [
     'available_markets',
     'images',
@@ -23,81 +16,68 @@ function cleanSpotify(data) {
     'is_local',
     'primary_color',
     'video_thumbnail',
-  ];
+  ]
 
-  function recurse (data) {
-    for (var key in data) {
+  const recurse = data => {
+    for (const key in data) {
       if (keys.includes(key)) {
-        delete data[key];
+        delete data[key]
+        continue
       }
-      else if (data[key] === 'name') {
-        data[key] = data[key].toLowerCase();
+      if (data[key] === 'name') {
+        data[key] = data[key].toLowerCase()
+        continue
       }
-      else {
-        switch(typeof data[key]) {
-          case 'object':
-            recurse(data[key]);
-            break;
-        }
+      if (typeof data[key] == 'object') {
+        recurse(data[key])
       }
     }
   }
-  recurse(data);
-  return data;
+
+  recurse(data)
+  return data
 }
 
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-axios.defaults.headers.put['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.headers.put['Content-Type'] = 'application/json'
 
 const lastfm = axios.create({
   baseURL: 'http://ws.audioscrobbler.com/2.0/',
-  transformResponse: [
-    JSON.parse,
-    cleanLastfm,
-  ],
-});
+  transformResponse: [JSON.parse, cleanLastfm],
+})
 
-lastfm.api_key = '434afecdf99ff904cc11c2bbfdb2d373';
+lastfm.api_key = '434afecdf99ff904cc11c2bbfdb2d373'
 
 async function init() {
-
-  const Authorization = await getAuth();
+  const Authorization = await getAuth()
 
   const spotify = axios.create({
     baseURL: 'https://api.spotify.com/v1/',
     headers: { Authorization },
-    transformResponse: [
-      data => data? JSON.parse(data): {},
-      cleanSpotify,
-    ]
-  });
+    transformResponse: [data => (data ? JSON.parse(data) : {}), cleanSpotify],
+  })
 
-  const get = (url, params) => spotify
-  .get(url, { params })
-  .then(({ data }) => data);
+  const get = (url, params) =>
+    spotify.get(url, { params }).then(({ data }) => data)
 
-  const post = (url, body, params) => spotify
-    .post(url, body, { params })
-    .then(({ data }) => data);
+  const post = (url, body, params) =>
+    spotify.post(url, body, { params }).then(({ data }) => data)
 
-  const put = (url, body, params) => spotify
-    .put(url, body, { params })
-    .then(({ data }) => data);
+  const put = (url, body, params) =>
+    spotify.put(url, body, { params }).then(({ data }) => data)
 
-  const del = (url, params) => spotify
-    .delete(url, { params })
-    .then(({ data }) => data);
-  
+  const del = (url, params) =>
+    spotify.delete(url, { params }).then(({ data }) => data)
+
   return {
     get,
     post,
     put,
     del,
     lastfm,
-  };
-
+  }
 }
 
 module.exports = {
   init,
-};
+}
